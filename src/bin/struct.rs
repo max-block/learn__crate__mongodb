78f1792ea3b76326d28f2886ecd850134d450c22
Max Block <maxblock@pm.me>
2020-09-22 11:20:00 +0300

    update

--- src/bin/struct.rs
+++ src/bin/struct.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 
-
 use bson::oid::ObjectId;
 use chrono::{DateTime, Utc};
 use mongodb::bson::{self, doc};
@@ -14,17 +13,43 @@ struct Data {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     id: Option<ObjectId>,
     name: String,
+    status: DataStatus,
     value: i64,
     tags: Vec<String>,
+    children: Vec<Child>,
     created_at: DateTime<Utc>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Child {
+    name: String,
+    group: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum DataStatus {
+    NEW,
+    OK,
+    ERROR,
 }
 
 fn insert_data(col: &Collection) -> Result<InsertOneResult, MongoError> {
     let new_data = Data {
         id: None,
         name: "n1".to_string(),
+        status: DataStatus::NEW,
         value: 10,
         tags: vec!["a1".to_string(), "b2".to_string()],
+        children: vec![
+            Child {
+                name: "c1".to_string(),
+                group: "g1".to_string(),
+            },
+            Child {
+                name: "c2".to_string(),
+                group: "g2".to_string(),
+            },
+        ],
         created_at: Utc::now(),
     };
     let new_data = bson::to_bson(&new_data)?;
@@ -50,6 +75,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     insert_data(&col)?;
 
     let one_data = find_one(&col);
-    println!("{:?}", one_data);
+    println!("{:#?}", one_data);
     Ok(())
 }
