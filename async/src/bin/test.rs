376b40c6bb148ab0947a1bd4229ee4ba17c4d4b9
Max Block <maxblock@pm.me>
2022-05-06 18:34:57 +0300

    update

--- async/src/bin/test.rs
+++ async/src/bin/test.rs
@@ -1,4 +1,12 @@
+use std::io::Error;
+
+use mongodb::{Client, options::ClientOptions};
+
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
+    let database_url = "mongodb://localhost/learn__crate__mongodb";
+    let client = Client::with_uri_str(database_url);
+    let database_name = ClientOptions::parse(database_url).await?.default_database.unwrap();
+    dbg!(database_name);
     Ok(())
 }
